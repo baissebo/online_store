a5b8621c8f97e09173cdd3a632b27c7deee15a55
Alexander Gedz <gedz.buh@yandex.ru>
2024-06-04 15:50:42 +0300

    added display of one product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_list
+from catalog.views import home, contacts, products_list, products_details
 
 
 app_name = CatalogConfig.name
@@ -8,5 +8,6 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('base/', products_list, name='products_list')
+    path('base/', products_list, name='products_list'),
+    path('products/<int:pk>/', products_details, name='products_details')
 ]
