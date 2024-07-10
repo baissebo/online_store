7a9fc7417520a24f82529526f54de742ba4b177a
Alexander Gedz <gedz.buh@yandex.ru>
2024-07-10 13:59:16 +0300

    task 1: made settings for working with redis

--- config/settings.py
+++ config/settings.py
@@ -113,3 +113,13 @@ AUTH_USER_MODEL = 'users.User'
 
 LOGIN_REDIRECT_URL = '/users/profile/'
 LOGOUT_REDIRECT_URL = '/users/login/'
+
+CACHE_ENABLED = True
+
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://localhost:6379'
+        }
+    }
